8bb19f5dedf46a085004e7873bf929d5d8a293c8
özgür aykutalp <prof1998@hotmail.com>
2025-09-23 23:47:50 +0300

    token

--- reddit.py
+++ reddit.py
@@ -166,6 +166,7 @@ def get_latest_json():
         return json.load(f)
 
 @reddit_bp.route("/latest", methods=["GET"])
+@token_required
 def get_latest_memes():
     data = get_latest_json()
     if data is None:
